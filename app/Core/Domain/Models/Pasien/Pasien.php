a224b26ac0f732d2a8b66ecaeabc866dba254629
Rencist <dafianakhdan@gmail.com>
2022-12-31 20:08:26 +0700

    feat: pasien model

---
+++ app/Core/Domain/Models/Pasien/Pasien.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Core\Domain\Models\Pasien;
+
+use Exception;
+use App\Core\Domain\Models\Email;
+use App\Exceptions\PasienException;
+use Illuminate\Support\Facades\Hash;
+
+class Pasien
+{
+    private PasienId $id;
+    private string $name;
+    private string $no_telp;
+    private string $alamat;
+    private string $foto;
+
+    /**
+     * @param PasienId $id
+     * @param string $name
+     * @param string $no_telp
+     * @param string $alamat
+     * @param string $foto
+     */
+    public function __construct(PasienId $id, string $name, string $no_telp, string $alamat, string $foto)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->no_telp = $no_telp;
+        $this->alamat = $alamat;
+        $this->foto = $foto;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public static function create(string $name, string $no_telp, string $alamat, string $foto): self
+    {
+        return new self(
+            PasienId::generate(),
+            $name,
+            $no_telp,
+            $alamat,
+            $foto,
+        );
+    }
+
+    /**
+     * @return PasienId
+     */
+    public function getId(): PasienId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAlamat(): string
+    {
+        return $this->alamat;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNoTelp(): string
+    {
+        return $this->no_telp;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFoto(): string
+    {
+        return $this->foto;
+    }
+}
---
+++ app/Core/Domain/Models/Pasien/PasienId.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Core\Domain\Models\Pasien;
+
+use App\Core\Domain\Models\UuidTrait;
+
+class PasienId
+{
+    use UuidTrait;
+}
