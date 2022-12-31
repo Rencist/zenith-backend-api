0fab51b763fc600c118b004ae9a02ffe601f7449
Rencist <dafianakhdan@gmail.com>
2022-12-31 20:03:38 +0700

    feat: pasien migration

---
+++ database/migrations/2022_12_30_143723_create_pasien_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pasien', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name', 128);
+            $table->string('no_telp', 32);
+            $table->string('alamat', 256);
+            $table->string('foto');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pasien');
+    }
+};
